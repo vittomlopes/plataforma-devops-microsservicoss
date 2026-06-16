9600eb5684a55357eabdbd32b829a8a5ed016e44
Vitor Lopes <137175471+vittomlopes@users.noreply.github.com>
2026-06-16 08:47:03 -0300

    Update app.py

--- product-service/app.py
+++ product-service/app.py
@@ -1,4 +1,28 @@
+from flask import Flask, jsonify
+import requests
+
+app = Flask(__name__)
+
+@app.route("/products")
+def products():
+    return jsonify([
+        {
+            "id": 1,
+            "nome": "Calculo Numerico"
+        }
+    ])
+
 @app.route("/user-info")
 def user_info():
-    response = requests.get("http://user-service:5000/users")
-    return response.json()
+    try:
+        response = requests.get("http://user-service:5000/users")
+        return response.json()
+    except:
+        return {"erro": "user-service indisponivel"}
+
+@app.route("/health")
+def health():
+    return jsonify({"status": "OK"})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5001)
