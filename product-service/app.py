7a432639d39689bddac72b632c97cdb88fb7efd7
Vitor Lopes <137175471+vittomlopes@users.noreply.github.com>
2026-06-16 07:59:13 -0300

    Update app.py

--- product-service/app.py
+++ product-service/app.py
@@ -2,12 +2,12 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route("/users")
-def users():
+@app.route("/products")
+def products():
     return jsonify([
         {
             "id": 1,
-            "nome": "Joao Vitor"
+            "nome": "Calculo Numerico"
         }
     ])
 
@@ -16,4 +16,4 @@ def health():
     return jsonify({"status": "OK"})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)print("Product Service")
+    app.run(host="0.0.0.0", port=5001)
