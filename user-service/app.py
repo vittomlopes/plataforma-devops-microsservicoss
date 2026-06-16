fbaf1c3aa93482275df91887e04085cdea74112d
Vitor Lopes <137175471+vittomlopes@users.noreply.github.com>
2026-06-16 07:55:32 -0300

    Implement basic Flask user service with health check

--- user-service/app.py
+++ user-service/app.py
@@ -1,1 +1,19 @@
-print("User Service1")
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/users")
+def users():
+    return jsonify([
+        {
+            "id": 1,
+            "nome": "Joao Vitor"
+        }
+    ])
+
+@app.route("/health")
+def health():
+    return jsonify({"status": "ok"})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)print("User Service1")
