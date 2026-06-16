4111d44da1088649c5d5bc5ed93dff392d2182fc
Vitor Lopes <137175471+vittomlopes@users.noreply.github.com>
2026-06-16 08:32:21 -0300

    Initialize Flask app with orders and health routes

--- order-service/app.py
+++ order-service/app.py
@@ -1,1 +1,19 @@
-print("Order Service")
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/orders")
+def orders():
+    return jsonify([
+        {
+            "id": 1,
+            "status": "aberto"
+        }
+    ])
+
+@app.route("/health")
+def health():
+    return jsonify({"status": "OK"})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5002)
